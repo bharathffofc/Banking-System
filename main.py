c27115438f354034198fbc60d4eda68e09c85baa
AIPLBLRNT0016\Bharath KA <bharath.ka@anorasolutions.com>
2025-04-11 17:15:14 +0530

    Changed the code

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def account_creation(n,name,password,initial_deposit):
             fieldnames = ['Account Number', 'Name', 'Password', 'Initial Deposit']
             write = csv.DictWriter(f, fieldnames=fieldnames)
             write.writerow({'Account Number':n, 'Name':name, 'Password':password, 'Initial Deposit':initial_deposit})
+            print("Account Created Successfully.")
+            with open("statement.csv","a",newline='') as file:
+                file.write(f"{n},Account Creation,{initial_deposit}\n")
     else:
         print("Insufficient deposit amount...")
 
@@ -29,6 +32,8 @@ def balance(n):
         res=csv.DictReader(f)
         for r in res:
             if n==int(r["Account Number"]):
+                with open("statement.csv", "a", newline='') as file:
+                    file.write(f"{n},Balance,{int(r["Initial Deposit"])}\n")
                 return f"Balance: {int(r["Initial Deposit"])}"
         return "Wrong Account Number..."
 
@@ -40,6 +45,8 @@ def deposit(n,deposit_amount):
             if n==int(r["Account Number"]):
                 df=True
                 r["Initial Deposit"]=str(int(r["Initial Deposit"])+deposit_amount)
+                with open("statement.csv", "a", newline='') as file:
+                    file.write(f"{n},Deposit,{deposit_amount}\n")
         if df:
             with open("data.csv","w",newline='') as f:
                 fieldnames=['Account Number', 'Name', 'Password', 'Initial Deposit']
@@ -58,6 +65,8 @@ def withdraw(n,withdraw_amount,):
                 if int(r["Account Number"])==n and int(r["Initial Deposit"])>withdraw_amount+100:
                     wf=True
                     r["Initial Deposit"]=str(int(r["Initial Deposit"])-withdraw_amount)
+                    with open("statement.csv", "a", newline='') as file:
+                        file.write(f"{n},Withdraw,{withdraw_amount}\n")
 
             if wf:
                 with open("data.csv","w",newline='') as f:
@@ -72,33 +81,48 @@ def withdraw(n,withdraw_amount,):
 def transfer(n1,n2,amount):
     sf = False
     rf = False
-    with open("data.csv","r",newline='') as f:
-        res=list(csv.DictReader(f))
-        for r in res:
-            if n1==int(r["Account Number"]) and int(r["Initial Deposit"])>amount+100:
-                sf=True
-                r["Initial Deposit"]=str(int(r["Initial Deposit"])-amount)
-        if sf:
+    if amount>0:
+        with open("data.csv","r",newline='') as f:
+            res=list(csv.DictReader(f))
             for r in res:
-                if n2==int(r["Account Number"]):
-                    rf=True
-                    r["Initial Deposit"]=str(int(r["Initial Deposit"])+amount)
-            if rf:
-                with open("data.csv","w",newline='') as f:
-                    fieldnames = ['Account Number', 'Name', 'Password', 'Initial Deposit']
-                    write = csv.DictWriter(f,fieldnames=fieldnames)
-                    write.writeheader()
-                    write.writerows(res)
-                return f"Amount {amount} is debited from {n1} and credited to {n2}"
+                if n1==int(r["Account Number"]) and int(r["Initial Deposit"])>amount+100:
+                    sf=True
+                    r["Initial Deposit"]=str(int(r["Initial Deposit"])-amount)
+            if sf:
+                for r in res:
+                    if n2==int(r["Account Number"]):
+                        rf=True
+                        r["Initial Deposit"]=str(int(r["Initial Deposit"])+amount)
+                        with open("statement.csv", "a", newline='') as file:
+                            file.write(f"{n1},Transfer from {n1} to {n2},{amount}\n")
+                if rf:
+                    with open("data.csv","w",newline='') as f:
+                        fieldnames = ['Account Number', 'Name', 'Password', 'Initial Deposit']
+                        write = csv.DictWriter(f,fieldnames=fieldnames)
+                        write.writeheader()
+                        write.writerows(res)
+                    return f"Amount {amount} is debited from {n1} and credited to {n2}"
+                else:
+                    return "Invalid Account Number"
             else:
-                return "Invalid Account Number"
-        else:
-            return "Sending amount is Greater than balance ot invalid Account Number"
+                return "Sending amount is Greater than balance ot invalid Account Number"
+    else:
+        print("Invalid Amount,Amount Should be Greater than 0")
 
+def statement(n):
+    with open("statement.csv","r",newline='') as file:
+        res=list(csv.DictReader(file))
+        ans=[]
+        for r in res:
+            if int(r["Account Number"])==n:
+                ans.append(f"{r["Account Number"]},{r["Operation"]},{r["Amount"]}")
+        return ans
 if __name__ == '__main__':
     acc_num=1
     with open("data.csv","w",newline='') as f:
         f.write("Account Number,Name,Password,Initial Deposit\n")
+    with open("statement.csv", "w", newline='') as file:
+        file.write("Account Number,Operation,Amount\n")
     while True:
         print("Enter Your Choice: ")
         print("1 for Account Creation.")
@@ -120,11 +144,12 @@ if __name__ == '__main__':
                 if msg:
                     print("Login Successful")
                     while True:
-                        print("1 for Check Balance.")
+                        print("\n1 for Check Balance.")
                         print("2 for Deposit.")
                         print("3 for Withdraw.")
                         print("4 for Transfer.")
-                        print("5 for Logging Out.")
+                        print("5 for Bank statement.")
+                        print("6 for Logging Out.\n")
                         choice=int(input())
                         match choice:
                             case 1:
@@ -148,6 +173,12 @@ if __name__ == '__main__':
                                 print(m)
 
                             case 5:
+                                res=statement(acc_num)
+                                print("Bank Statement: \n")
+                                for r in res:
+                                    print(r)
+
+                            case 6:
                                 print("Logging Out...")
                                 break
                 else:
@@ -156,3 +187,20 @@ if __name__ == '__main__':
             case 0:
                 print("Exit the Program...")
                 break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
